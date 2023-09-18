e5bf42940d68a5b7e86adac26732ed4e8a09cf14
Ojidaemo <vit.martsinovich@gmail.com>
2023-09-18 18:01:28 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Vitali Martsinovich on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
